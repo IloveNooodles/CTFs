cb54f0839fe5c5ba4120c47557e9a708ed2c29cd
ILoveNooodles <mgarebaldhie80@gmail.com>
2023-09-27 19:51:56 +0000

    feat: update template

--- pwn_template.py
+++ pwn_template.py
@@ -1,6 +1,7 @@
 from pwn import *
 
-CONN = "".split(" ")
+# Pass the nc hostname port
+CONN = "nc localhost 1337".split(" ")
 HOST = CONN[1]
 PORT = CONN[2]
 
@@ -18,62 +19,65 @@ def cl(): io.clean()
 def un64(x): return u64(x.ljust(8, b'\x00'))
 def leak(name, addr): info(f"{name} @ {hex(addr)}")
 
-# ===========================================================
-#                           SETUP
-# ===========================================================
-
-# Allows you to switch between local/GDB/remote from terminal
 def start(argv=[], *a, **kw):
-    if args.GDB:  # Set GDBscript below
+    if args.GDB:  
         return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
-    elif args.REMOTE:  # ('server', 'port')
+    elif args.REMOTE:  
         return remote(HOST, PORT, *a, **kw)
-    else:  # Run locally
+    else:  
         return process([exe] + argv, *a, **kw)
 
-
-# Specify GDB script here (breakpoints etc)
-gdbscript = """
-c
-""".format(
-    **locals()
-)
-
-# Binary filename
-exe = "./vuln"
-
-# This will automatically get context arch, bits, os etc
-elf = context.binary = ELF(exe, checksec=False)
-rop = ROP(elf)
-
-# Change logging level to help with debugging (error/warning/info/debug)
-context.terminal = "tmux splitw -h".split(" ")
-context.log_level = "debug"
-
-# Lib-C library, can use pwninit/patchelf to patch binary
-libc = elf.libc
-# ld = ELF("/lib64/ld-linux-x86-64.so.2", checksec=False)
-
-if args.REMOTE:
-    pass
-    # libc = ELF("/lib/x86_64-linux-gnu/libc.so.6", checksec=False)
-    # ld = ELF("/lib64/ld-linux-x86-64.so.2", checksec=False)
 
 # ===========================================================
 #                    EXPLOIT GOES HERE
 # ===========================================================
 
-offset = 64
+def exploit(io):
+    offset = 64
 
-# Start program
-io = start()
+    # Build the payload
+    payload = flat({offset: []})
 
-# Build the payload
-payload = flat({offset: []})
+    # Send the payload
+    sla(b"> ", payload)
+    ru(b"Thank you!")
 
-# Send the payload
-sla(b"> ", payload)
-ru(b"Thank you!")
+    # Got Shell?
+    io.interactive()
 
-# Got Shell?
-io.interactive()
+# ===========================================================
+#                           SETUP
+# ===========================================================
+
+if __name__ == "__main__":
+    
+    # Specify GDB script here (breakpoints etc)
+    gdbscript = """
+    c
+    """.format(
+        **locals()
+    )
+    
+    # Binary filename
+    exe = "./vuln"
+    
+    # This will automatically get context arch, bits, os etc
+    elf = context.binary = ELF(exe, checksec=False)
+    rop = ROP(elf)
+    
+    # Change logging level to help with debugging (error/warning/info/debug)
+    context.terminal = "tmux splitw -h".split(" ")
+    context.log_level = "debug"
+    
+    # Lib-C library, can use pwninit/patchelf to patch binary
+    libc = elf.libc
+    ld = ELF("/lib64/ld-linux-x86-64.so.2", checksec=False)
+    
+    if args.REMOTE:
+        pass
+        # libc = ELF("libc.so.6", checksec=False)
+        # ld = ELF("ld-linux-x86-64.so.2", checksec=False)
+    
+    io = start()
+
+    exploit(io)
